88896a53a9a7dde678adfa4d3e675baf3de1c55e
Saugat Joshi <jsaugatt02@gmail.com>
2024-11-24 15:02:53 +0545

    cors fixes

--- server/src/app.js
+++ server/src/app.js
@@ -6,12 +6,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: ['https://trackex-client.vercel.app', process.env.BASE_URL],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 // requests come from different sources like url, json, body/ form
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
