3fd845328b00fb25ecc309c26b968343acc06c4f
Saugat Joshi <saugatec@gmail.com>
2026-03-05 11:33:12 +0545

    formatting

--- server/src/controllers/topStats.js
+++ server/src/controllers/topStats.js
@@ -1,5 +1,5 @@
-import asyncHandler from 'express-async-handler';
-import Revenue from '../models/Revenue.js';
+import asyncHandler from "express-async-handler";
+import Revenue from "../models/Revenue.js";
 import AppError from "../utils/appError.js";
 
 //? GET top customers by revenue
@@ -9,18 +9,18 @@ export const getTopCustomers = asyncHandler(async (req, res) => {
     throw new AppError("Unauthorized user", 401);
   }
   const topCustomers = await Revenue.aggregate([
-    { $match: { type: 'revenue', organization: organizationId } },
+    { $match: { type: "revenue", organization: organizationId } },
     {
       $group: {
         _id: {
           $cond: {
-            if: { $eq: ['$customer', ''] },
-            then: 'Unknown',
-            else: '$customer'
-          }
+            if: { $eq: ["$customer", ""] },
+            then: "Unknown",
+            else: "$customer",
+          },
         },
-        totalRevenue: { $sum: '$amount' }
-      }
+        totalRevenue: { $sum: "$amount" },
+      },
     },
     { $sort: { totalRevenue: -1 } },
     { $limit: 30 },
@@ -35,14 +35,18 @@ export const getTopProductsByQuantity = asyncHandler(async (req, res) => {
     throw new AppError("Unauthorized user", 401);
   }
   const topProductsByQuantity = await Revenue.aggregate([
-    { $match: { type: 'revenue', organization: organizationId } },
-    { $group: { _id: { description: '$description', category: '$category' }, totalQuantity: { $sum: '$quantity' } } },
+    { $match: { type: "revenue", organization: organizationId } },
+    {
+      $group: {
+        _id: { description: "$description", category: "$category" },
+        totalQuantity: { $sum: "$quantity" },
+      },
+    },
     { $sort: { totalQuantity: -1 } },
     { $limit: 30 },
   ]);
   res.status(200).json(topProductsByQuantity);
 });
-
 
 //? GET top selling products by revenue
 export const getTopProductsByRevenue = asyncHandler(async (req, res) => {
@@ -51,57 +55,75 @@ export const getTopProductsByRevenue = asyncHandler(async (req, res) => {
     throw new AppError("Unauthorized user", 401);
   }
   const topProductsByRevenue = await Revenue.aggregate([
-    { $match: { type: 'revenue', organization: organizationId } },
-    { $group: { _id: { description: '$description', category: '$category' }, totalRevenue: { $sum: '$amount' } } },
-    { $sort: { totalRevenue: -1 } },
-    { $limit: 30 },
+    {
+      $match: {
+        type: "revenue",
+        organization: organizationId,
+      },
+    },
+    {
+      $group: {
+        _id: {
+          description: "$description",
+          category: "$category",
+        },
+        totalRevenue: {
+          $sum: "$amount",
+        },
+      },
+    },
+    {
+      $sort: {
+        totalRevenue: -1,
+      },
+    },
+    { $limit: 100 },
   ]);
 
   res.status(200).json(topProductsByRevenue);
 });
 
 //? GET top categories by revenue
-// GET top categories by revenue
 export const getTopCategoriesByRevenue = asyncHandler(async (req, res) => {
   const organizationId = req.user?.organization;
   if (!organizationId) {
     throw new AppError("Unauthorized user", 401);
   }
   const topCategoriesByRevenue = await Revenue.aggregate([
-    { $match: { type: 'revenue', organization: organizationId } },
-    { $group: { _id: '$category', totalRevenue: { $sum: '$amount' } } },
+    { $match: { type: "revenue", organization: organizationId } },
+    { $group: { _id: "$category", totalRevenue: { $sum: "$amount" } } },
     { $sort: { totalRevenue: -1 } },
     { $limit: 30 },
     {
       $lookup: {
-        from: 'categories', // name of the categories collection
-        let: { categoryName: '$_id' },
+        from: "categories", // name of the categories collection
+        let: { categoryName: "$_id" },
         pipeline: [
           {
             $match: {
               $expr: {
                 $and: [
-                  { $eq: ['$name', '$$categoryName'] },
-                  { $eq: ['$organization', organizationId] },
+                  { $eq: ["$name", "$$categoryName"] },
+                  { $eq: ["$organization", organizationId] },
                 ],
               },
             },
           },
         ],
-        as: 'categoryDetails',
+        as: "categoryDetails",
       },
     },
     {
-      $unwind: '$categoryDetails'
+      $unwind: "$categoryDetails",
     },
     {
       $project: {
         _id: 0,
-        category: '$_id',
+        category: "$_id",
         totalRevenue: 1,
-        icon: '$categoryDetails.icon'
-      }
-    }
+        icon: "$categoryDetails.icon",
+      },
+    },
   ]);
 
   res.status(200).json(topCategoriesByRevenue);
